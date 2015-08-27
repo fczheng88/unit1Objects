8244b1194e965514b43e436d20ca20a40c98b824
fczheng88 <fczheng88@gmail.com>
2015-08-27 14:12:07 -0500

    Chapter 2 practice

---
+++ Chapter 2 Practice/string1.java
@@ -0,0 +1,9 @@
+public class string1
+{
+    public static void main( String[] args)
+    {
+        String greeting = "hello, World!";
+        int length = greeting.length();
+        System.out.println("length = " + length);
+    }
+}
