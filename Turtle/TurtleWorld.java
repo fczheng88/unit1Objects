c96ca185017140683972f543c8bb1a551cafa1b4
fczheng88 <fczheng88@gmail.com>
2015-09-01 15:32:05 -0500

    Turtle Lab

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -3,60 +3,63 @@ import java.util.Random;
 
 public class TurtleWorld
 {
+    public static Color makeColor(Random rand)
+    {   //Creates and returns a random color
+        float r = rand.nextFloat();
+        float g = rand.nextFloat();
+        float b = rand.nextFloat();
+        Color color = new Color(r,g,b);
+        return color;
+    }
     public static void main(String args[])
+    throws InterruptedException
     {
        
-        World turtleWorld = new World();
+        World turtleWorld = new World(700,700);
         Turtle turtle = new Turtle(turtleWorld);
         Turtle turtle1 = new Turtle(turtleWorld);
-        //Turtle turtle2 = new Turtle(turtleWorld);
-        //Turtle turtle3 = new Turtle(turtleWorld);
-        turtle.turn(0);
-        //turtle1.turn(180);
-        //turtle2.turn(180);
-        //turtle3.turn(270);
-        turtle.setPenColor(Color.BLUE);
-        //turtle1.setPenColor(Color.RED);
-        //turtle2.setPenColor(Color.GREEN);
-        //turtle3.setPenColor(Color.YELLOW);
-        turtle.penDown();
-        //turtle1.penDown();
-        //turtle2.penDown();
-        //turtle3.penDown();
+        Turtle turtle2 = new Turtle(turtleWorld);
+        Turtle turtle3 = new Turtle(turtleWorld);
         Random rand = new Random();
-        for(int i=1; i<101; i++){
-            //float r = rand.nextFloat();
-            //float g = rand.nextFloat();
-            //float b = rand.nextFloat();
-            //Color randColor = new Color(r,g,b);
+        int howBig = 1;
+        int iterations = 1000;
+        int angle = 129;
+        
+        turtle.turn(0);
+        turtle1.turn(angle);
+        turtle2.turn(2*angle);
+        turtle3.turn(3*angle);
+        turtle.setPenColor(makeColor(rand));
+        turtle1.setPenColor(makeColor(rand));
+        turtle2.setPenColor(makeColor(rand));
+        turtle3.setPenColor(makeColor(rand));
+        turtle.penDown();
+        turtle1.penDown();
+        turtle2.penDown();
+        turtle3.penDown();
+        
+        
+        for(int i=1; i<=iterations; i++){
+            Thread.sleep(50);
             
-            if(i%2==0){
-                turtle.penUp();
-            }
-            //if((i+1)%2==0){
-            //    turtle1.penUp();
-            //}
-            //if((i+2)%2==0){
-            //    turtle2.penUp();
-            //}
-            //if((i+3)%2==0){
-            //    turtle3.penUp();
-            //}
+            turtle.setPenColor(makeColor(rand));
+            turtle.forward(i*howBig);
+            turtle.turn(angle);
             
-            //turtle.setPenColor(randColor);
-            turtle.forward(i*5);
-            turtle.turn(90);
-            turtle.penDown();
-            //turtle1.forward(i*5-5);
-            //turtle1.turn(90);
-            //turtle1.penDown();
-            //turtle2.forward(i*5);
-            //turtle2.turn(45);
-            //turtle2.penDown();
-            //turtle3.forward(i*5);
-            //turtle3.turn(45);
-            //turtle3.penDown();
+            turtle1.setPenColor(makeColor(rand));
+            turtle1.forward(i*howBig);
+            turtle1.turn(angle);
             
+            turtle2.setPenColor(makeColor(rand));
+            turtle2.forward(i*howBig);
+            turtle2.turn(angle);
+            
+            turtle3.setPenColor(makeColor(rand));
+            turtle3.forward(i*howBig);
+            turtle3.turn(angle);
+            
+            
+                       
             
             
         }
