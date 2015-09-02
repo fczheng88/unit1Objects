5a5e7dec35f2742662aa421a92f33b3c06bb22d4
fczheng88 <fczheng88@gmail.com>
2015-09-02 14:14:58 -0500

    asdlfka

--- Chapter 2 Practice/DieSimulator.java
+++ Chapter 2 Practice/DieSimulator.java
@@ -3,12 +3,31 @@ import java.util.Random;
 public class DieSimulator
 {
     public static void main(String[] args)
-    {
+    {   
+        int rolls = 5;
+        //Option A
         Random rnd = new Random();
         
-        for(int i=1;i<=5;i++){
-            int die = rnd.nextInt(6);
+        for(int i=1;i<=rolls;i++){
+            int die = rnd.nextInt(6)+1; //number between 0 and 5 and add 1
             System.out.println(die);
         }
+        
+        //Option B
+        double die2 = Math.random(); //double between 0 and 1
+        for(int i=1;i<=rolls;i++){
+            die2 = Math.random();
+            die2 += 1;
+            
+            System.out.println((int)(die2));
+        }
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -2,62 +2,89 @@ import java.awt.Color;
 import java.util.Random;
 
 public class TurtleWorld
-{
-    public static void main(String args[])
+{   
+    public static Color makeColor()
     {
-       
-        World turtleWorld = new World();
+        Random rand = new Random();
+        float r = rand.nextFloat();
+        float g = rand.nextFloat();
+        float b = rand.nextFloat();
+        Color randColor = new Color(r,g,b);
+        return randColor;
+    }
+    public static boolean turtleOutOfBounds(Turtle turtle, int worldX, int worldY)
+    {
+        if(Math.abs(turtle.getXPos()) > worldX)
+        {
+            System.out.println("xpos");
+            return false;
+        }
+        if(Math.abs(turtle.getYPos()) > worldY)
+        {
+            System.out.println("ypos");
+            return false;
+        }
+        return true;
+        
+    }
+    public static void main(String args[])
+    throws InterruptedException
+    {
+        
+    int angle = 121;
+    int length = 1;
+    int iter = 1000;
+    int delay = 50;
+    int incwidth = 75;
+        
+    int worldX = 1000;
+    int worldY = 700;       
+     
+        
+        World turtleWorld = new World(worldX,worldY);
         Turtle turtle = new Turtle(turtleWorld);
         Turtle turtle1 = new Turtle(turtleWorld);
-        //Turtle turtle2 = new Turtle(turtleWorld);
-        //Turtle turtle3 = new Turtle(turtleWorld);
-        turtle.turn(0);
-        //turtle1.turn(180);
-        //turtle2.turn(180);
-        //turtle3.turn(270);
-        turtle.setPenColor(Color.BLUE);
-        //turtle1.setPenColor(Color.RED);
-        //turtle2.setPenColor(Color.GREEN);
-        //turtle3.setPenColor(Color.YELLOW);
+        Turtle turtle2 = new Turtle(turtleWorld);
+        Turtle turtle3 = new Turtle(turtleWorld);
+                
+        turtle.turn(0*angle);
+        turtle1.turn(angle);
+        turtle2.turn(2*angle);
+        turtle3.turn(3*angle);
+        turtle.setPenColor(makeColor());
+        turtle1.setPenColor(makeColor());
+        turtle2.setPenColor(makeColor());
+        turtle3.setPenColor(makeColor());
         turtle.penDown();
-        //turtle1.penDown();
-        //turtle2.penDown();
-        //turtle3.penDown();
+        turtle1.penDown();
+        turtle2.penDown();
+        turtle3.penDown();
         Random rand = new Random();
-        for(int i=1; i<101; i++){
-            //float r = rand.nextFloat();
-            //float g = rand.nextFloat();
-            //float b = rand.nextFloat();
-            //Color randColor = new Color(r,g,b);
-            
-            if(i%2==0){
-                turtle.penUp();
+        for(int i=1; i<=iter; i++){
+            if(turtleOutOfBounds(turtle, worldX, worldY))
+            {
+                break;
             }
-            //if((i+1)%2==0){
-            //    turtle1.penUp();
-            //}
-            //if((i+2)%2==0){
-            //    turtle2.penUp();
-            //}
-            //if((i+3)%2==0){
-            //    turtle3.penUp();
-            //}
+            System.out.println(turtle.getYPos());
+            System.out.println(worldY);
+            turtle.setPenColor(makeColor());
+            turtle.forward(i*length);
+            turtle.turn(angle);
+            turtle.setPenWidth(i/incwidth);
+            turtle1.setPenColor(makeColor());
+            turtle1.forward(i*length);
+            turtle1.turn(angle);
+            turtle1.setPenWidth(i/incwidth);
+            turtle2.setPenColor(makeColor());
+            turtle2.forward(i*length);
+            turtle2.turn(angle);
+            turtle2.setPenWidth(i/incwidth);
+            turtle3.setPenColor(makeColor());
+            turtle3.forward(i*length);
+            turtle3.turn(angle);
+            turtle3.setPenWidth(i/incwidth);
             
-            //turtle.setPenColor(randColor);
-            turtle.forward(i*5);
-            turtle.turn(90);
-            turtle.penDown();
-            //turtle1.forward(i*5-5);
-            //turtle1.turn(90);
-            //turtle1.penDown();
-            //turtle2.forward(i*5);
-            //turtle2.turn(45);
-            //turtle2.penDown();
-            //turtle3.forward(i*5);
-            //turtle3.turn(45);
-            //turtle3.penDown();
-            
-            
+            Thread.sleep(delay);            
             
         }
     }
